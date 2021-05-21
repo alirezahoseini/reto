5c7983029d50678ca5eb28df15abc369e94459cd
alireza <mehdimj0161@gmail.com>
2021-05-21 23:52:30 +0430

    added login form

--- js/main.js
+++ js/main.js
@@ -10,17 +10,18 @@ darkButton.addEventListener("click", function () {
 const menuButton = document.getElementById("menu-button");
 const hambergerMenu = document.getElementById("hamberger-menu");
 const menuCloserBtn1 = document.getElementById("menu-closer-1");
-const hambergerMenuBgFillter = document.getElementById("hamberger-menu-background-fillter");
+const hambergerMenuBgFillter = document.getElementById(
+	"hamberger-menu-background-fillter",
+);
 menuButton.addEventListener("click", function () {
 	hambergerMenu.classList.add("active");
 	hambergerMenuBgFillter.classList.add("active");
-
 });
 menuCloserBtn1.addEventListener("click", function () {
 	hambergerMenu.classList.remove("active");
 	hambergerMenuBgFillter.classList.remove("active");
 });
-hambergerMenuBgFillter.addEventListener("click",function(){
+hambergerMenuBgFillter.addEventListener("click", function () {
 	hambergerMenu.classList.remove("active");
 	hambergerMenuBgFillter.classList.remove("active");
 });
@@ -30,7 +31,22 @@ hambergerMenuBgFillter.addEventListener("click",function(){
 // loading button
 const loadingButton = document.getElementsByClassName("loadMoreBtn")[0];
 const lodingGif = document.getElementsByClassName("loadingGif")[0];
-	loadingButton.addEventListener("click", function(){
-		loadingButton.classList.toggle("active")
-	});
+loadingButton.addEventListener("click", function () {
+	loadingButton.classList.toggle("active");
+});
 //----End of loading button
+
+// login form
+const loginBtn = document.getElementsByClassName("login-button")[0];
+const loginFillter = document.getElementById("login-fillter");
+const loginBase = document.getElementsByClassName("login-base")[0];
+loginBtn.addEventListener("click", function () {
+	loginFillter.classList.add("active");
+	loginBase.classList.add("active");
+	
+});
+loginFillter.addEventListener("click", function () {
+	loginFillter.classList.remove("active");
+	loginBase.classList.remove("active");
+});
+//----End of login form
