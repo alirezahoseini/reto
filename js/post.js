a7b762659ce3b493d6deebc3989738cd76fcf4e8
alireza <mehdimj0161@gmail.com>
2021-05-25 19:47:06 +0430

    added lightbox to post page

---
+++ js/post.js
@@ -0,0 +1,19 @@
+const myImages = ["../images/home-page-images/1.jpg"];
+const lightboxFillter = document.getElementsByClassName("lightbox-fillter")[0];
+const lightboxBase = document.getElementsByClassName("lightbox-base")[0];
+const imageSelector = document.getElementById("image");
+
+imageSelector.addEventListener("click", function(){
+    lightboxBase.classList.add("active");
+	lightboxFillter.classList.add("active");
+});
+
+lightboxFillter.addEventListener("click", function(){
+	lightboxBase.classList.remove("active");
+	lightboxFillter.classList.remove("active");
+});
+
+lightboxBase.addEventListener("click", function(){
+	lightboxBase.classList.remove("active");
+	lightboxFillter.classList.remove("active");
+});
